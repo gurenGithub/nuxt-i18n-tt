f00c25c94698bed94d19d78cd64e21e9f229c497
panwj <panwj@xunbai.onaliyun.com>
2022-04-19 12:18:51 +0800

    增加说明文档

--- scripts/translate/index.configs.js
+++ scripts/translate/index.configs.js
@@ -1,12 +1,12 @@
 module.exports = {
-  translates: [{ dir: "./../../pages", reg: "**/**.vue" }],
-  locales: ["en", "zh-CN"],
+  translates: [{ dir: "./../../pages", reg: "**/**.vue" }], //需要转译文件
+  locales: ["en", "zh-CN"], //配置生成语言包
 
   regs: {
     test(obj) {
-      return /.*[\u4e00-\u9fa5]+.*$/.test(obj);
+      return /.*[\u4e00-\u9fa5]+.*$/.test(obj); //验证提取出来文字是否包含中文
     },
-    translates: [
+    translates: [  //提取转译规则
       {
         match: () => /"##[\u4e00-\u9fa5，！~【】a-zA-Z0-9]+"|/gi,
         extract: () => `app.$tt('$key','[$value]')`,
@@ -37,7 +37,7 @@ module.exports = {
       },
     ],
 
-    translateds: [
+    translateds: [  //提取已经转译文件匹配文件写入到语言包
       {
         match() {
           return /\$tt\(['"](\w+)['"](,|, )['"]\[([\u4e00-\u9fa5，！~【】a-zA-Z]+)\]['"]\)/gi;
